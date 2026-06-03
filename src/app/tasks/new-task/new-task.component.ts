6b23713b23df479967c42fc720d3c4d253c3d7e9
unknown <saurabh955920@gmail.com>
2026-06-03 23:40:22 +0530

    Using Angular's Dependency Injection Machanism

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -12,10 +12,25 @@ import { TasksService } from '../tasks.service';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
-  private taskService :TasksService;
-  constructor(){
-      this.taskService = new  TasksService();
-  }
+  /*
+    private taskService :TasksService;
+    constructor(tasksService : TasksService){
+        this.taskService =  tasksService;
+    }
+  */
+ /*
+    constructor(private tasksService : TasksService){
+    }
+  */
+
+  private tasksService : TasksService = inject(TasksService);
+
+  /*
+    This is the way of Dependency Injection, in this in each class no new instance will be created. We give this job to Angular so that it creates object that is shared across all classes and for each class no neew instance is craeated, rather shared instance is created across all classes
+    -All 3 approaches mentioned above creates shared instance across all classes.
+  */
+
+
 
   onAddTask(title: string, description: string) {
     this.taskService.addTask({title,description});
