a3b7c421ae51a79b82d933523cdc2a62c88652a2
unknown <saurabh955920@gmail.com>
2026-06-04 00:24:01 +0530

    Outsourcing & Reusing Logic with Services

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,29 +1,31 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-
-
   private tasks = signal<Task[]>([]);
-  allTasks = this.tasks.asReadonly();
-  
-  constructor() {
-      
+ allTasks = this.tasks.asReadonly();
+
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => {
+      const newArr = [...oldTasks, newTask];
+      return newArr;
+    });
   }
 
-  addTask(taskData :{title:string;description:string}){
-    const newTask : Task = {
-        ...taskData,
-        id : Math.random().toString(),
-        status:'OPEN'
-    }
-    this.tasks.update((oldTasks)=>{
-        const newArr = [...oldTasks,newTask];
-        return  newArr;
-    })
-
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
